89fc702a59fd8531a82bf32a3b7917613308bda0
Rohith Kumar Marise <rohithmarise@gmail.com>
2023-08-03 10:56:03 +0530

    Variable and functions difference

---
+++ 04-function.sh
@@ -0,0 +1,9 @@
+# Variable and Functions are similar
+# If we assign a name to certain data then it is called variable
+# If we assign a name to certain commands then it is called function
+
+welcom() {
+  echo "Hello good day !"
+}
+# We have to declare function first before using it
+welcom

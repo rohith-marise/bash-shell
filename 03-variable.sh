66ad2c2ceaf1be558f336c32a61d5a60bd104af7
Rohith Kumar Marise <rohithmarise@gmail.com>
2023-08-11 18:44:59 +0530

    RabbitMQ NFR

--- 03-variable.sh
+++ 03-variable.sh
@@ -1,2 +1,11 @@
 a=10
 echo ${a} #This prints a value = 10
+Date=$(date)
+# Normal parentheses if for to execute/assign a command to variable
+# Follower bracket parentheses if we want to do command substitution
+# Timestamp=${Date}
+echo "Today date is ${Date}"
+
+## Arithmetic Substitution
+ADD=$(( 2+2 ))
+echo ADD of 2+2 = ${ADD}

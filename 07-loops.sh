1d45afb4951443b38d6563b63a13e2ca47eca5f3
Rohith Kumar Marise <rohithmarise@gmail.com>
2023-08-22 23:03:29 +0530

    for loop

--- 07-loops.sh
+++ 07-loops.sh
@@ -8,3 +8,10 @@ while [ ${a} -gt 0 ] ; do
   a=$(( ${a} - 1 ))
   #break # This command can break the loop
 done
+
+# For loop script make sure you have a names.txt file with some names
+#for i in $(cat names.txt) ; do
+# echo "Hi ${i}"
+#done
+#
+#please give this script in for loop in different forms

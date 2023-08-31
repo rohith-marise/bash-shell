e98d2936f67222b33d9dc54f266ba9690aa9cacf
Rohith Kumar Marise <rohithmarise@gmail.com>
2023-09-01 02:45:31 +0530

    Shell script practice

--- 09-demo_shipping.sh
+++ 09-demo_shipping.sh
@@ -1,7 +1,11 @@
-
-Date=$(date +%Y%m%d)
-Dir=/tmp
-Log_file=$Dir.$0.$Date.log
+DATE=$(date +%F)
+LOGSDIR=/tmp
+# /home/centos/shellscript-logs/script-name-date.log
+SCRIPT_NAME=$0
+LOGFILE=$LOGSDIR/$0-$DATE.log
+#Date=$(date +%Y%m%d)
+#Dir=/tmp
+#LOGFILE=$Dir.$0.$Date.log
 User_id=$(id -u)
 R="\e[31m"
 G="\e[32m"
@@ -22,52 +26,52 @@ Validate() {
 }
 
 
-yum install maven -y &>>$Log_file
+yum install maven -y &>>$LOGFILE
 Validate $? "Installing maven package"
 
-id roboshop &>>$Log_file
+id roboshop &>>$LOGFILE
 if [ $? -ne 0 ] ; then
-useradd roboshop &>>$Log_file
+useradd roboshop &>>$LOGFILE
 Validate $? "Adding roboshop user"
 fi
 
 if [ ! -e /app ] ; then
-mkdir /app &>>$Log_file
+mkdir /app &>>$LOGFILE
 Validate $? "Creating app directory"
 fi
 
 
-curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip &>>$Log_file
+curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip &>>$LOGFILE
 Validate $? "Downloading shipping component content"
 
 
-cd /app &>>$Log_file
-unzip /tmp/shipping.zip &>>$Log_file
+cd /app &>>$LOGFILE
+unzip /tmp/shipping.zip &>>$LOGFILE
 Validate $? "Extracting shipping component content"
 
-mvn clean package &>>$Log_file
+mvn clean package &>>$LOGFILE
 Validate $? "Building shipping component"
 
-mv target/shipping-1.0.jar shipping.jar &>>$Log_file
+mv target/shipping-1.0.jar shipping.jar &>>$LOGFILE
 Validate $? "Moving built shipping component"
 
-mv /home/centos/learn-shell/shipping.service /etc/systemd/system/shipping.service &>>$Log_file
+mv /home/centos/learn-shell/shipping.service /etc/systemd/system/shipping.service &>>$LOGFILE
 Validate $? "Creating an shipping service file"
 
-systemctl daemon-reload &>>$Log_file
+systemctl daemon-reload &>>$LOGFILE
 Validate $? "Reloading the serive files "
 
-systemctl enable shipping &>>$Log_file
+systemctl enable shipping &>>$LOGFILE
 Validate $? "Enableing the shipping component service "
 
-systemctl start shipping &>>$Log_file
+systemctl start shipping &>>$LOGFILE
 Validate $? "start shipping component"
 
-yum install mysql -y &>>$Log_file
+yum install mysql -y &>>$LOGFILE
 Validate $? "Insatlling my sql"
 
-mysql -h <MYSQL-SERVER-IPADDRESS> -uroot -pRoboShop@1 < /app/schema/shipping.sql &>>$Log_file
+mysql -h <MYSQL-SERVER-IPADDRESS> -uroot -pRoboShop@1 < /app/schema/shipping.sql &>>$LOGFILE
 Validate $? "Loading the schema"
 
-systemctl restart shipping &>>$Log_file
+systemctl restart shipping &>>$LOGFILE
 Validate $? "Restart shipping service "

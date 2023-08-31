6da5d995adfe860d0dcf808c70f727fc486cd51b
Rohith Kumar Marise <rohithmarise@gmail.com>
2023-09-01 03:51:37 +0530

    Shell script practice

--- 03-variable.sh
+++ 03-variable.sh
@@ -2,6 +2,8 @@ a=10
 echo ${a} #This prints a value = 10
 Date=$(date)
 # Normal parentheses if for to execute/assign a command to variable
+#Date=$(date +%F)
+#echo "${Date} or $(date +%F)" both are same
 # Follower bracket parentheses if we want to do command substitution
 # Timestamp=${Date}
 echo "Today date is ${Date}"

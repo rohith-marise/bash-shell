0435470d6ef74fc49528db56767d424125ec8652
Rohith Kumar Marise <rohithmarise@gmail.com>
2023-12-31 04:16:36 +0530

    test

--- 07-loops.sh
+++ 07-loops.sh
@@ -2,12 +2,12 @@
 # For and while loops
 
 #  Based on expression
-a=10
-while [ ${a} -gt 0 ] ; do
-  echo hello
-  a=$(( ${a} - 1 ))
-  #break # This command can break the loop
-done
+#a=10
+#while [ ${a} -gt 0 ] ; do
+#  echo hello
+#  a=$(( ${a} - 1 ))
+#  #break # This command can break the loop
+#done
 
 # For loop script make sure you have a names.txt file with some names
 #for i in $(cat names.txt) ; do
